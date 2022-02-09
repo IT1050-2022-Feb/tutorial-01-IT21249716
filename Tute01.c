5e8d9e5f3c204dbe0be0f3181fc1679c7251e9c3
IT21249716 <99313683+IT21249716@users.noreply.github.com>
2022-02-09 11:48:30 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+   int value1,value2,value3,sum,avg;
+   
+   printf("Enter the mark for subject 1: ");
+   scanf("%d",&value1);
+   printf("Enter the mark for subject 2: ");
+   scanf("%d",&value2);
+   printf("Enter the mark for subject 3: ");
+   scanf("%d",&value3);
+   
+   sum = value1 + value2;
+   avg = sum/2 
+   printf("Average mark  of the student)
   
   return 0;
 }
